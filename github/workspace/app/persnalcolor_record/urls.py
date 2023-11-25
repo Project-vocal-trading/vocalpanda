ba6f98ab644f414ca7efa54509c1e5f09a587ef8
Jong-BinPark <68490334+Jong-BinPark@users.noreply.github.com>
2023-11-26 06:52:44 +0900

    del

--- github/workspace/app/persnalcolor_record/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.record, name='persnalcolor_record'),
-    path('save_recording/', views.save_recording, name='save_recording'),
-]
--- github/workspace/app/vp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
-
-# Create your views here.
-def mainpage(request):
-  return render(request, 'mainpage/mainpage.html')
-
-def error_404_view(request, exception):
-  return HttpResponseNotFound(f'지금 페이지는 없는 페이지입니다.')
